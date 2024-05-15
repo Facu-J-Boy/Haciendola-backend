1e4fe3faf92d44d538b39754a217721d20fef2f9
Facu-J-Boy <boyfacundo45@gmail.com>
2024-05-14 23:22:46 -0300

    feat: product description dataType TEXT

--- src/models/Product.js
+++ src/models/Product.js
@@ -19,7 +19,7 @@ const Product = sequelize.define(
       allowNull: false,
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false,
     },
     SKU: {
