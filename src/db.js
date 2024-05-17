5dcc1ead2b9387511897557186cd3284f960215f
Facu-J-Boy <boyfacundo45@gmail.com>
2024-05-16 22:02:03 -0300

    feat: db settings

--- src/db.js
+++ src/db.js
@@ -4,9 +4,12 @@ require('dotenv').config();
 
 const { DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'postgres', // Puedes cambiarlo según el dialecto que estés usando
-});
+const sequelize = new Sequelize(
+  'postgres://ogzwmchg:9y2Zjt0M1uCp7X1q2EkJF2FWbkuN7Vmb@bubble.db.elephantsql.com/ogzwmchg',
+  {
+    host: 'localhost',
+    dialect: 'postgres', // Puedes cambiarlo según el dialecto que estés usando
+  }
+);
 
 module.exports = sequelize;
